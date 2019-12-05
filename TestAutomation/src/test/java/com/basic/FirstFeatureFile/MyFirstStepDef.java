388b27005a5cf67d211255f703c3eecc28c7c42c
Lachezar Kolarov <Lachezar@flabon.com>
2019-12-05 21:51:03 +0100

    Second commit

--- TestAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ TestAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -19,6 +19,7 @@ public class MyFirstStepDef {
 		System.setProperty("webdriver.chrome.driver", "C:\\CucumberSetup\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
+		System.out.println("opening URL");
 		
 	}
 	
